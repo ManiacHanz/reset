80c47dd79f3f84b5be93999086942516422a7aeb
ManiacHanz <182490078@qq.com>
2022-02-22 22:31:59 +0800

    Patch: init husky to protect pre-commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+	render(<App />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
+});
+
+it("test 1 plus 1 should be 2", () => {
+	expect(1 + 1).toBe(2);
 });
